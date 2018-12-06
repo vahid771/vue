8821244c2d7c312518cb208f8be1a384a773f758
vahed mafi <vahid771@gmail.com>
2018-12-06 23:09:27 +0330

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" :src="`${baseUrl}logo.png`">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  data () {
+    return {
+      baseUrl: process.env.BASE_URL,
+	  image: 'a.jpg'
+    }
+  },
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
