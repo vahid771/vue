8a002c3ac7b4682aaffc0aa23f2ac4c39eef4264
vahid771 <vahid771@gmail.com>
2018-12-12 10:05:57 +0330

    authentication server added

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/upvote">Upvote</router-link>
+      <router-link to="/upvote">Upvote</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link> |
+      <router-link to="/dashboard">Dashboard</router-link> |
+      <router-link to="/admin">Admin</router-link>
     </div>
     <router-view/>
   </div>
--- src/router.js
+++ src/router.js
@@ -1,29 +1,92 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import('./views/Home.vue')
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import('./views/About.vue')
     },
     {
       path: '/upvote',
       name: 'upvote',
       component: () => import('./views/Upvote.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/Login.vue'),
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('./views/Register.vue'),
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/dashboard',
+      name: 'userboard',
+      component: () => import('./views/UserBoard.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('./views/Admin.vue'),
+      meta: {
+        requiresAuth: true,
+        is_admin: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('jwt') == null) {
+      next({
+        path: '/login',
+        params: { nextUrl: to.fullPath }
+      })
+    } else {
+      let user = JSON.parse(localStorage.getItem('user'))
+      if (to.matched.some(record => record.meta.is_admin)) {
+        if (user.is_admin == 1) {
+          next()
+        }
+        else {
+          next({ name: 'userboard' })
+        }
+      } else {
+        next()
+      }
+    }
+  } else if (to.matched.some(record => record.meta.guest)) {
+    if (localStorage.getItem('jwt') == null) {
+      next()
+    }
+    else {
+      next({ name: 'userboard' })
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
